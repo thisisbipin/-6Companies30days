ef16a9cb900084eeae447d6510e97f638f4f107d
Bipin Jadav <55138889+thisisbipin@users.noreply.github.com>
2022-01-06 09:29:02 +0530

    Create 2 - Overlapping rectangles.cpp

---
+++ Goldman Sachs/2 - Overlapping rectangles.cpp
@@ -0,0 +1,16 @@
+// Given top left and bottom right corners of two rectangles, determine if they overlap.
+
+class Solution {
+public:
+
+    int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+      int res=1; // Let's Assume they overlap  
+      
+      // Cases where our assumptions may fail
+      if (L2[0] > R1[0] || L1[0] > R2[0] || L1[1] < R2[1] || L2[1] < R1[1])
+            res=0;
+      
+      // return the result
+        return res;
+    }
+};
